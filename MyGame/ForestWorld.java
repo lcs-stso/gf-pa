e99201f85b72306073e92d0638ae2ab02639a8c0
Sarah Tso <stso.lcs.on.ca>
2019-04-02 11:39:55 -0400

    Change the name of world to ForestWorld

--- MyGame/ForestWorld.java
+++ MyGame/ForestWorld.java
@@ -1,21 +1,23 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class ForestWorld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class ForestWorld extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class ForestWorld.
      * 
      */
-    public MyWorld()
+    public ForestWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        
     }
 }
