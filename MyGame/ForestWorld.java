5d2b0e732b78ab39ac748471faa351b408dca99d
Sarah Tso <stso.lcs.on.ca>
2019-04-03 19:30:21 -0400

    add the cat in the screen

--- MyGame/ForestWorld.java
+++ MyGame/ForestWorld.java
@@ -18,6 +18,10 @@ public class ForestWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         
+        //Create a cat in the screen 
+        Cat theCat = new Cat();
+        this.addObject(theCat,300,250);
+        
         
     }
     public void act()
